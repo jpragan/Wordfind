08e899fec7109d8124e9a42e0983222a5e0026de
johnragan <jpragan16@gmail.com>
2021-03-09 22:14:35 -0500

    API now returns definition to word. Wordfind Prints definition with real english words.

--- index.js
+++ index.js
@@ -5,30 +5,15 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 exports.handler = async function(event, context) {
 
     let word = "";
-    var isword = false;
+    var isword = undefined;
     var length = undefined;
-    let responseBody;
     let response;
+    let definition;
+    var result = "{\"Words\":[]}";
+    var resultobj = JSON.parse(result);
 
-    //get word from query string
-    if (event.queryStringParameters && event.queryStringParameters.word) {
-        console.log("Received word: " + event.queryStringParameters.word);
-        word = event.queryStringParameters.word;
-    }
-
-    // All words in database have capital first letter and wont match if first letter isn't capitalized.
-  word = word.charAt(0).toUpperCase() + word.slice(1)
-
-    //set up parameter to send to DynamoDB
-    var params = {
-        TableName: 'Dictionary',
-        KeyConditionExpression: 'Word = :hashKey',
-        ExpressionAttributeValues: {
-            ':hashKey': word
-        }
-    };
-
-// call to dynamoDB to check if the word exists or not
+    // checks DyanmoDB dictionary for word
+    // if data.items.length > 0 a word exists in the dictionary
     function checkDictionary() {
         return new Promise(resolve => {
             dynamoDB.query(params,
@@ -37,42 +22,65 @@ exports.handler = async function(event, context) {
                         console.error("Unable to query. Error:");
                     }
                     else {
-                        length = data.Items.length; // length is the number of definitions for a word that is returned
-                        resolve(length)             // length > 0 indicates that the word exists.
+                        length = data.Items.length;
+                        console.log("data items - " + JSON.stringify(data.Items))
+                        if (length > 0)
+                            definition = data.Items[0]["Definition"]
+                        resolve(length)
                     }
                 })
         })
     }
 
-// these calls needed to be asynchronous due to the dynamoDB call.
     async function asyncCall() {
         const result = await checkDictionary();
-        //console.log(result);
         createResponse()
         return result
     }
 
-    await asyncCall();
+    for (let i = 0, wlength = event.length; i < wlength; i++) {
+        word = JSON.stringify(event[i].Word)
+        word = word.replace(/"/g, "");
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+        var params = {
+            TableName: 'Dictionary',
+            KeyConditionExpression: 'Word = :hashKey',
+            ExpressionAttributeValues: {
+                ':hashKey': word
+            }
+        };
+
+        await asyncCall();
+
+        if (isword){
+            resultobj['Words'].push({"Word": word, "is_word": isword, "Definition": definition});
+        }else{
+            resultobj['Words'].push({"Word": word, "is_word": isword});
+        }
+
+    }
 
 
-    //build the response
     function createResponse() {
         let responseCode = 200;
         if (length > 0) {
             isword = true;
         }
-        responseBody = {
-            is_word: isword,
-        };
-        response = {
-            statusCode: responseCode,
-            headers: {
-                "Words Checked": word
-            },
-            body: JSON.stringify(responseBody)
-        };
+        else {
+            isword = false;
+        }
+        response =
+            {
+                statusCode: responseCode,
+                headers: {
+                    "Words Checked": word
+                },
+                body: resultobj
+            };
     }
 
+    createResponse();
+    // console.log("response - " + JSON.stringify(obj));
     return response;
 
 };
